98ba90bd08ce435478e8f0d9630d953677d36bba
houbin <houbin-12@163.com>
2015-03-25 16:36:12 +0800

    添加文件

---
+++ src/define.php
@@ -0,0 +1,21 @@
+<?php
+
+/* @brief 用于常量定义
+ */
+
+$menu = array(
+	"首页"=> ("#", 1), 
+	"新闻"=> ("news.html", 2),
+	"新闻"=> ("news.html", 3),
+	"新闻"=> ("news.html", 4),
+	"新闻"=> ("news.html", 5),
+	"新闻"=> ("news.html", 6)
+);
+
+$sub_menu == array(
+	"1" => array(),
+	"2" => array(
+		"201"=>"国内", 
+		"201"=>"国际", 
+	)
+);

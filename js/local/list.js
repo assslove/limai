99c75b276eae15f6221c4bac498a1b81775d9f6e
houbin <houbin-12@163.com>
2015-03-26 12:40:04 +0800

    增加上传文件接口

--- js/local/list.js
+++ js/local/list.js
@@ -1,3 +1,19 @@
+function send_file(file, editor, welEditable) {
+	data = new FormData();
+	data.append("file", file);
+	$.ajax({
+		data: data,
+		type: "POST",
+		url: "src/upload.php",
+		cache: false,
+		contentType: false,
+		processData: false,
+		success: function(url) {
+			editor.insertImage(welEditable, url);
+		}
+	});
+}
+
 $(document).ready(function() {
 	$('#myTab a').click(function (e) {
 		e.preventDefault()
@@ -6,8 +22,12 @@ $(document).ready(function() {
 
 	$('.click2edit').summernote({
 		height: "500px",
-		focus: true
+		focus: true,
+		onImageUpload : function(files, editor, welEditable) {
+			send_file(files[0], editor, welEditable);
+		}
 	});
+
 });
 
 function list() 
@@ -22,8 +42,8 @@ function list()
 
 function save() 
 {
-  var aHTML = $('.click2edit').code();
-  alert(aHTML);
-  $('.click2edit').destroy();
+	var aHTML = $('.click2edit').code();
+	alert(aHTML);
+	$('.click2edit').destroy();
 
 }
--- src/upload.php
+++ src/upload.php
@@ -1,1 +1,26 @@
+<?php
 
+function upload() 
+{
+	if ($_FILES['file']['name']) {
+		if (!$_FILES['file']['error']) {
+			$name = md5(rand(100, 200));
+			$ext = explode('.', $_FILES['file']['name']);
+			$filename = $name . '.' . $ext[1];
+			$destination = '../img/upload/' . $filename; 
+			$location = $_FILES["file"]["tmp_name"];
+			if (move_uploaded_file($location, $destination)) {
+				echo 'img/upload/' . $filename;
+			} else {
+				echo "移动到指定目录出错";
+			}
+		} else {
+			echo  $message = '上传出错:  '.$_FILES['file']['error'];
+		}
+	}
+}
+
+
+upload();
+
+?>
